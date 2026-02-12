0121eb1f069d84715913cdfa8c40ab1486e43af6
smounade <140447843+smounade@users.noreply.github.com>
2026-02-12 14:41:30 +0100

    Add Seat class with occupant management methods

--- utils/table.py
+++ utils/table.py
@@ -1,1 +1,21 @@
+class Seat:
+    def __init__(self, free: bool, occupant: str = None):
+        self.free = free
+        self.occupant = occupant
 
+    def set_occupant(self, name: str):
+        if self.free == True:
+            self.occupant = name
+            self.free = False
+        else:
+            raise Exception("Someone is already sitting here")
+
+    def remove_occupant(self):
+        if self.free == False:
+            return_value=self.occupant
+            self.occupant = None
+            self.free = True
+            return return_value
+        else:
+            raise Exception("This seat is free")
+          
