c420c977d004e727b099fa9defdf5a35607e1d36
smounade <140447843+smounade@users.noreply.github.com>
2026-02-12 17:13:52 +0100

    Add Openspace class with table initialization

--- utils/openspace.py
+++ utils/openspace.py
@@ -1,1 +1,9 @@
+from table.py import Table
 
+class Openspace:
+
+    def __init__(self, tables: list, number_of_tables: int = 6):
+        self.tables=tables
+        self.number_of_tables=number_of_tables
+
+  
