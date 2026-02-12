b5bdf5e602299b2c65c74883d1dd9e5fbd777193
smounade <140447843+smounade@users.noreply.github.com>
2026-02-12 14:51:03 +0100

    Add type annotations to occupant methods
    Add return type annotations to set_occupant and remove_occupant methods.

--- utils/table.py
+++ utils/table.py
@@ -3,14 +3,14 @@ class Seat:
         self.free = free
         self.occupant = occupant
 
-    def set_occupant(self, name: str):
+    def set_occupant(self, name: str) -> None:
         if self.free:
             self.occupant = name
             self.free = False
         else:
             raise Exception("Someone is already sitting here")
 
-    def remove_occupant(self):
+    def remove_occupant(self) -> str:
         if not self.free:
             return_value=self.occupant
             self.occupant = None
