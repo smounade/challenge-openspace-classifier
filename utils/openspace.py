22687ced85a79ca915b08522483255f44a279080
smounade <140447843+smounade@users.noreply.github.com>
2026-02-13 09:13:42 +0100

    Implement organize method in Openspace class
    Added organize method to randomly assign names to tables.

--- utils/openspace.py
+++ utils/openspace.py
@@ -1,9 +1,13 @@
 from table.py import Table
+import random
 
 class Openspace:
 
     def __init__(self, tables: list, number_of_tables: int = 6):
         self.tables=tables
         self.number_of_tables=number_of_tables
-
   
+    def organize(self, names: list):
+        for name in names:
+            table=random.choice(self.tables)
+            
