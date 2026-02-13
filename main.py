1c4f5fe2bc5bb60843d5ff72f0baec16aba6463c
smounade <140447843+smounade@users.noreply.github.com>
2026-02-13 10:19:24 +0100

    Switch from Excel to CSV file handling
    Replaced Excel file handling with CSV file processing.

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-filename = "./utils/data.xlsx"
-with open(filename, "r") as my_file:
-    for line in my_file:
+import csv
+
+filename = "./utils/data.csv"
+
+with open(filename, newline='', encoding='utf-8') as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        print(row)
         #write your code here
 
 with open("colleagues.csv", "w", encoding="utf-8-sig") as file:
