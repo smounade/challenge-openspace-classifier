14bb3c547660c529b19425d825d0e3c054081cb9
smounade <140447843+smounade@users.noreply.github.com>
2026-02-13 10:22:09 +0100

    Integrate Openspace to organize CSV data
    Added Openspace instance and organized CSV rows.

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import csv
 
-filename = "./utils/data.csv"
+openspace1=Openspace()
 
+filename = "./utils/data.csv"
 with open(filename, newline='', encoding='utf-8') as csvfile:
     reader = csv.reader(csvfile)
     for row in reader:
-        print(row)
+        openspace1.organize(row)
         #write your code here
 
 with open("colleagues.csv", "w", encoding="utf-8-sig") as file:
