626dd66b74228e364361bf9fbf9e3a858a42210a
smounade <140447843+smounade@users.noreply.github.com>
2026-02-13 10:08:01 +0100

    Add functionality to write new colleagues to CSV

--- main.py
+++ main.py
@@ -2,4 +2,8 @@ filename = "./utils/data.xlsx"
 with open(filename, "r") as my_file:
     for line in my_file:
         #write your code here
-        
+
+with open("colleagues.csv", "w", encoding="utf-8-sig") as file:
+    file.write("New Colleagues\n")
+    for name in new_collegues:
+        file.write(name + "\n")
